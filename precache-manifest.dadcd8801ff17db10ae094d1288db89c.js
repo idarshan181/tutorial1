87e916220fe2af9badc6e7a5a00ea9d40524ae7f
idarshan181 <ddpatel97@gmail.com>
2020-02-19 15:25:18 +0530

    Updates

---
+++ precache-manifest.dadcd8801ff17db10ae094d1288db89c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35cea37dcaa6f9d0979faa100a18c46a",
+    "url": "/tutorial1/index.html"
+  },
+  {
+    "revision": "939e3ba5a49e9c23f49e",
+    "url": "/tutorial1/static/css/main.7c0be19a.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/tutorial1/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tutorial1/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "939e3ba5a49e9c23f49e",
+    "url": "/tutorial1/static/js/main.9e795d49.chunk.js"
+  },
+  {
+    "revision": "9184af410da101021919",
+    "url": "/tutorial1/static/js/runtime-main.0bc8e44d.js"
+  }
+]);
